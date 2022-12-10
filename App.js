347621ade9e8c0073fc8592f91aef65291631106
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-12-10 13:50:57 -0500

    writ e AppPicker, AppTextInput and P ickerItem components

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import React from "react";
-import { Screen } from "react-native";
+import React, { useState } from "react";
 
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
+import AppPicker from "./app/components/AppPicker";
+import AppTextInput from "./app/components/AppTextInput";
+import Screen from "./app/components/Screen";
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
 
 export default function App() {
-  return <ListingsScreen />;
+  const [category, setCategory] = useState(categories[0]);
+
+  return (
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
+  );
 }
