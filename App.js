bcbcc5d2a621a46a76326cdcab1074c887ec3bd2
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-12-26 21:47:40 -0500

    add notifications when contacting seller with expo-notificaitons

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import AuthContext from "./app/auth/context";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import authStorage from "./app/auth/storage";
 import navigationTheme from "./app/navigation/navigationTheme";
+import { navigationRef } from "./app/navigation/rootNavigation";
 import OfflineNotice from "./app/components/OfflineNotice";
 
 export default function App() {
@@ -23,7 +24,7 @@ export default function App() {
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
-      <NavigationContainer theme={navigationTheme}>
+      <NavigationContainer ref={navigationRef} theme={navigationTheme}>
         {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
     </AuthContext.Provider>
